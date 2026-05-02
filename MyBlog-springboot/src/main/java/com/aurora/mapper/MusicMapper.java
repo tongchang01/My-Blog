fa077ff3a4a48db81b787e20b80630932997f846
TONGYIBIN <Tong-yibin@outlook.com>
2026-05-02 14:07:03 +0900

    XML配置修正

--- MyBlog-springboot/src/main/java/com/aurora/mapper/MusicMapper.java
+++ MyBlog-springboot/src/main/java/com/aurora/mapper/MusicMapper.java
@@ -5,6 +5,7 @@ import com.aurora.model.dto.MusicAdminDTO;
 import com.aurora.model.dto.MusicDTO;
 import com.aurora.model.vo.ConditionVO;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,8 +15,10 @@ public interface MusicMapper extends BaseMapper<Music> {
 
     List<MusicDTO> listMusics();
 
-    List<MusicAdminDTO> listBackMusics(Long current, Long size, ConditionVO conditionVO);
+    List<MusicAdminDTO> listBackMusics(@Param("current") Long current,
+                                       @Param("size") Long size,
+                                       @Param("conditionVO") ConditionVO conditionVO);
 
-    MusicAdminDTO getBackMusicById(Integer musicId);
+    MusicAdminDTO getBackMusicById(@Param("musicId") Integer musicId);
 
 }
