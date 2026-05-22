d826b3446477b66fd4457c167b62a1c1485c472d
TONGYIBIN <Tong-yibin@outlook.com>
2026-05-22 22:28:57 +0900

    build: scaffold backend v2 module

---
+++ MyBlog-springboot-v2/src/test/java/com/aurora/myblog/v2/MyBlogV2ApplicationTest.java
@@ -0,0 +1,14 @@
+package com.aurora.myblog.v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class MyBlogV2ApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
