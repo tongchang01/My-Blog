2e1399ffa1309a95173afca16a657df3c7cea892
TONGYIBIN <Tong-yibin@outlook.com>
2026-06-06 10:28:11 +0900

    使用Lombok精简审计实体基类

--- MyBlog-springboot-v2/src/main/java/com/tyb/myblog/v2/common/infrastructure/persistence/entity/BaseEntity.java
+++ MyBlog-springboot-v2/src/main/java/com/tyb/myblog/v2/common/infrastructure/persistence/entity/BaseEntity.java
@@ -2,20 +2,16 @@ package com.tyb.myblog.v2.common.infrastructure.persistence.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * 带雪花主键的标准业务实体基类。
  */
+@Getter
+@Setter
 public abstract class BaseEntity extends AuditOnlyBase {
 
     @TableId(type = IdType.ASSIGN_ID)
     private Long id;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
